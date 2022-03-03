b2677edb38988e438891ba31084e9a2c7d33d59e
Chris Josephs <chrismjosephs@gmail.com>
2022-03-03 15:46:55 -0800

    create working schemas for db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const router = require('./routes');
+const models = require('../db/models');
 
 const app = express();
 const PORT = 3000; // process.env.PORT
 
 app.use(express.json());
-app.use('/api', router);
+
+app.get('api/reviews', models);
+
+app.post('api/reviews', models);
+
+app.get('api/reviews/meta', models);
+
+app.put('api/reviews/:review_id/helpful', models);
+
+app.put('api/reviews/:review_id/report', models);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
