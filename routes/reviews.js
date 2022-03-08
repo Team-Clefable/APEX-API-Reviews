72ed83891d86c9ee62defc87531216991279a4cc
Chris Josephs <chrismjosephs@gmail.com>
2022-03-07 21:22:22 -0800

    Implement count and page parameter filtering

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,28 +1,36 @@
-// This file is gonna be fat
 const db = require('../db');
 
 module.exports = {
   getReview: async (req, res) => {
     const { product_id } = req.query;
-    const sortParam = req.query.sort;
     const page = req.query.page || 1;
     const count = req.query.count || 5;
+    const sortParam = req.query.sort || 'relevant';
     let sortQueryBy;
     switch (sortParam) {
       case 'helpful':
-        sortQueryBy = 'helpfulness';
+        sortQueryBy = 'helpfulness DESC';
         break;
       case 'newest':
-        sortQueryBy = 'date';
+        sortQueryBy = 'date DESC';
         break;
       case 'relevant':
-        sortQueryBy = 'helpfulness, date';
+        sortQueryBy = 'helpfulness DESC, date DESC';
         break;
       default:
         sortQueryBy = 'helpfulness';
     }
+    // Ask staff about this loop
+    const queryResultStartIndex = count * (page - 1);
+    const queryResultEndIndex = count * page;
 
     try {
+      const modifiedQueryObject = {
+        product: product_id,
+        page,
+        count,
+      };
+
       // postgres aggregation
       // const queryResult = await db.query(
       //   // 5.41 s
@@ -42,17 +50,24 @@ module.exports = {
       //   res.status(404).send('Oops! Looks like that product does not exist.');
       //   return;
       // }
-      // res.status(200).send(queryResult.rows);
+      // modifiedQueryObject.results = queryResult.rows.slice(
+      // queryResultStartIndex, queryResultEndIndex
+      // );
+      // res.status(200).send(modifiedQueryObject);
 
       // javascript aggregation
       const queryResult = await db.query(
         // 7.65 s
         // 4.92 s
+        // 1764 ms
+        // 1365 ms
+        // 1328 ms
         `
         SELECT r.review_id, r.rating, r.summary, r.recommend, r.response,
         r.body, r.date, r.reviewer_name, r.helpfulness
         FROM reviews r
         WHERE r.product_id = ${product_id}
+        ORDER BY ${sortQueryBy};
         `,
       );
 
@@ -71,11 +86,9 @@ module.exports = {
         `,
       );
 
-      const modifiedQueryObject = {
-        product: product_id,
-        page,
-        count,
-        results: queryResult.rows.map((queryRow) => (
+      modifiedQueryObject.results = queryResult.rows
+        .slice(queryResultStartIndex, queryResultEndIndex)
+        .map((queryRow) => (
           {
             review_id: queryRow.review_id,
             rating: queryRow.rating,
@@ -88,8 +101,7 @@ module.exports = {
             helpfulness: queryRow.helpfulness,
             photos: queryResultPhotos.rows,
           }
-        )),
-      };
+        ));
 
       res.status(200).send(modifiedQueryObject);
     } catch (err) {
@@ -100,25 +112,35 @@ module.exports = {
 
   // this one is gonna be fat
   postReview: async (req, res) => {
-    const {
-      product_id, // int
-      rating, // int
-      summary, // text
-      body, // text
-      recommend, // bool
-      name, // text
-      email, // text
-      photos, // array of text
-      characteristics // object {id: review_value}
-    } = req.body;
+    // const {
+    //   product_id, // int
+    //   rating, // int
+    //   summary, // text
+    //   body, // text
+    //   recommend, // bool
+    //   name, // text
+    //   email, // text
+    //   photos, // array of text
+    //   characteristics // object {id: review_value}
+    // } = req.body;
 
     try {
-      await db.query(
-        `
-        INSERT INTO reviews
-        VALUES
-        `,
-      );
+      // add new review row
+      // await db.query(
+      //   `
+      //   INSERT INTO reviews
+      //   VALUES
+      //   UPDATE product_characteristics_join j
+      //   SET
+      //     j.total_score = j.total_score + ${},
+      //     j.total_votes = j.total_votes + 1
+      //   WHERE
+      //   `,
+      // );
+      // update meta table
+      // insert into photos table with review id
+      // insert into characteristics reviews
+      // update product_characteristics_join
       res.sendStatus(201);
     } catch (err) {
       res.sendStatus(400);
