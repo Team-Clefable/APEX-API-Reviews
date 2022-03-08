6da69d596063e4ea300d592aeeb951fff91536dd
Chris Josephs <chrismjosephs@gmail.com>
2022-03-07 21:50:14 -0800

    comment out js aggregation, comment in pg aggregation

--- routes/reviews.js
+++ routes/reviews.js
@@ -32,80 +32,74 @@ module.exports = {
       };
 
       // postgres aggregation
-      // const queryResult = await db.query(
-      //   // 5.41 s
-      //   // 4.63 s
-      //   // 3.79 s
-      //   `SELECT r.review_id, rating,
-      //   summary, recommend, response, body, date,
-      //   reviewer_name, helpfulness, json_agg(json_build_object(
-      //     'id',p.id, 'url', p.photo_url
-      //     )) as photos
-      //   FROM reviews r LEFT JOIN photos p ON r.review_id = p.review_id
-      //   WHERE product_id = ${product_id}
-      //   GROUP BY r.review_id
-      //   ORDER BY ${sortQueryBy};`,
-      // );
-      // if (!queryResult.rows.length) {
-      //   res.status(404).send('Oops! Looks like that product does not exist.');
-      //   return;
-      // }
-      // modifiedQueryObject.results = queryResult.rows.slice(
-      // queryResultStartIndex, queryResultEndIndex
-      // );
-      // res.status(200).send(modifiedQueryObject);
-
-      // javascript aggregation
       const queryResult = await db.query(
-        // 7.65 s
-        // 4.92 s
-        // 1764 ms
-        // 1365 ms
-        // 1328 ms
-        `
-        SELECT r.review_id, r.rating, r.summary, r.recommend, r.response,
-        r.body, r.date, r.reviewer_name, r.helpfulness
-        FROM reviews r
-        WHERE r.product_id = ${product_id}
-        ORDER BY ${sortQueryBy};
-        `,
+        `SELECT r.review_id, rating,
+        summary, recommend, response, body, date,
+        reviewer_name, helpfulness, json_agg(json_build_object(
+          'id',p.id, 'url', p.photo_url
+          )) as photos
+        FROM reviews r LEFT JOIN photos p ON r.review_id = p.review_id
+        WHERE product_id = ${product_id}
+        GROUP BY r.review_id
+        ORDER BY ${sortQueryBy};`,
       );
-
       if (!queryResult.rows.length) {
         res.status(404).send('Oops! Looks like that product does not exist.');
         return;
       }
-
-      const queryResultPhotos = await db.query(
-        `
-        SELECT p.id, p.photo_url AS url
-        FROM photos p
-        JOIN reviews r
-        ON p.review_id = r.review_id
-        WHERE r.product_id = ${product_id}
-        `,
-      );
-
       modifiedQueryObject.results = queryResult.rows
-        .slice(queryResultStartIndex, queryResultEndIndex)
-        .map((queryRow) => (
-          {
-            review_id: queryRow.review_id,
-            rating: queryRow.rating,
-            summary: queryRow.summary,
-            recommend: queryRow.recommend,
-            response: queryRow.response,
-            body: queryRow.body,
-            date: queryRow.date,
-            reviewer_name: queryRow.reviewer_name,
-            helpfulness: queryRow.helpfulness,
-            photos: queryResultPhotos.rows,
-          }
-        ));
+        .slice(queryResultStartIndex, queryResultEndIndex);
+
+      // javascript aggregation
+      // const queryResult = await db.query(
+      //   `
+      //   SELECT r.review_id, r.rating, r.summary, r.recommend, r.response,
+      //   r.body, r.date, r.reviewer_name, r.helpfulness
+      //   FROM reviews r
+      //   WHERE r.product_id = ${product_id}
+      //   ORDER BY ${sortQueryBy};
+      //   `,
+      // );
+
+      // if (!queryResult.rows.length) {
+      //   res.status(404).send('Oops! Looks like that product does not exist.');
+      //   return;
+      // }
+
+      // const queryResultPhotos = await db.query(
+      //   `
+      //   SELECT p.id, p.photo_url AS url
+      //   FROM photos p
+      //   JOIN reviews r
+      //   ON p.review_id = r.review_id
+      //   WHERE r.product_id = ${product_id}
+      //   `,
+      // );
+
+      // modifiedQueryObject.results = queryResult.rows
+      //   .slice(queryResultStartIndex, queryResultEndIndex)
+      //   .map((queryRow) => (
+      //     {
+      //       review_id: queryRow.review_id,
+      //       rating: queryRow.rating,
+      //       summary: queryRow.summary,
+      //       recommend: queryRow.recommend,
+      //       response: queryRow.response,
+      //       body: queryRow.body,
+      //       date: queryRow.date,
+      //       reviewer_name: queryRow.reviewer_name,
+      //       helpfulness: queryRow.helpfulness,
+      //       photos: queryResultPhotos.rows,
+      //     }
+      //   ));
+
+      if (!modifiedQueryObject.results.length) {
+        res.status(404).send('Oops! Looks like there are no reviews on that page. Try a smaller page.');
+        return;
+      }
 
       res.status(200).send(modifiedQueryObject);
     } catch (err) {
-      // no product id sends a 422
       res.status(500).send(err);
     }
   },
