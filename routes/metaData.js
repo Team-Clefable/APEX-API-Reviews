cd633080cdf19d0fe0740f17d706861297c7d2ac
Chris Josephs <chrismjosephs@gmail.com>
2022-03-07 20:41:16 -0800

    write query for meta data

--- routes/metaData.js
+++ routes/metaData.js
@@ -2,18 +2,52 @@ const db = require('../db');
 
 module.exports = {
   getMetaData: async (req, res) => {
-    const { product_id } = req.params;
+    const { product_id } = req.query;
     try {
-      const result = db.query(
+      const queryResult = await db.query(
         `
-        SELECT m.product_id, m.ratings, m.recommended, c.characteristics
-        FROM meta m
-        LEFT JOIN product_characteristics_join c
-        WHERE m.product_id = ${product_id} and c.product_id = ${product_id}
+        SELECT *
+          FROM meta m
+          JOIN product_characteristics_join j
+          ON m.product_id = j.product_id
+          JOIN characteristics c
+          ON j.characteristic_id = c.id
+          WHERE m.product_id = ${product_id}
         `,
-        [],
       );
-      res.status(200).send(result.rows);
+
+      const { rows } = queryResult;
+      if (!rows.length) {
+        res.status(404).send('Oops! Looks like that product does not exist.');
+        return;
+      }
+
+      const characteristics = {};
+      // Ask staff about this loop
+      rows.forEach((queryObj) => {
+        characteristics[queryObj.name] = {
+          id: queryObj.id,
+          value: queryObj.total_score / queryObj.total_votes,
+        };
+      });
+
+      const formattedQueryResult = {
+        product_id: rows[0].product_id,
+        ratings: {
+          1: rows[0].rating_1,
+          2: rows[0].rating_2,
+          3: rows[0].rating_3,
+          4: rows[0].rating_4,
+          5: rows[0].rating_5,
+        },
+        recommended: {
+          false: rows[0].not_recommended,
+          true: rows[0].recommended,
+        },
+        characteristics,
+      };
+
+      res.status(200).send(formattedQueryResult);
     } catch (err) {
       res.status(500).send(err);
     }
