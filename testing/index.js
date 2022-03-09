6a59499625630d47c427d808b6e802587acab7ec
Chris Josephs <chrismjosephs@gmail.com>
2022-03-08 16:49:12 -0800

    write out k6 test

--- testing/index.js
+++ testing/index.js
@@ -1,16 +1,28 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { Counter } from 'k6/metrics';
 
 export const options = {
-  stages: [
-    // { duration: '30s', target: 20 },
-    // { duration: '1m30s', target: 10 },
-    { duration: '20s', target: 300 },
-  ],
+  vus: 20,
+  duration: '15s',
 };
 
+const randomNumber = (max, min) => (
+  Math.floor(Math.random() * (max - 1 + min) + min)
+);
+let count = randomNumber(1000000, 1);
+const metaUrl = `http://localhost:3000/api/reviews/meta?product_id=${count}`;
+// const reviewsUrl = `http://localhost:3000/api/reviews?product_id=${count}`;
+
 export default function () {
-  const res = http.get('http://localhost:3000/reviews/meta'); // drop in query params
-  check(res, { 'status was 200': (r) => r.status == 200 });
+  const res = http.get(metaUrl);
   sleep(1);
+  check(res, {
+    'status was 200': (r) => r.status === 200,
+    'transaction time< 200ms': (r) => r.timings.duration < 200,
+    'transaction time< 500ms': (r) => r.timings.duration < 500,
+    'transaction time< 1000ms': (r) => r.timings.duration < 1000,
+    'transaction time< 2000ms': (r) => r.timings.duration < 2000,
+  });
+  count = randomNumber(1000000, 1);
 }
