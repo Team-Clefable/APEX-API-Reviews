441c5d8c8086008770f3e94a284455288b7dc2d4
Chris Josephs <chrismjosephs@gmail.com>
2022-03-11 12:55:14 -0800

    replace router with direct path to controllers, og code commented out

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
 require('dotenv').config();
+
+const { getMetaData } = require('./routes/metaData');
+const {
+  getReviews, postReview, reportReview, markHelpful,
+} = require('./routes/reviews');
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
-app.use('/api/reviews', routes);
+// app.use('/api/reviews', routes);
+
+app.get('/api/reviews', getReviews);
+app.post('/api/reviews', postReview);
+
+app.get('/api/reviews/meta', getMetaData);
+
+app.put('/api/reviews/:review_id/report', reportReview);
+
+app.put('api/reviews/:review_id/helpful', markHelpful);
 
 app.get(`/${process.env.LOADER_IO_KEY}`, (req, res) => {
   res.send(process.env.LOADER_IO_KEY);
